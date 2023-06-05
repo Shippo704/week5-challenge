a0731dac59fff3e1c0e4e63a102fb7c0b520e0fa
Shippo704 <matt.taylor704@yahoo.ca>
2023-06-05 13:59:49 -0400

    added current date to header using dayjs and jquery

--- script.js
+++ script.js
@@ -12,20 +12,18 @@ var hour16 = $("#hour-16");
 var hour17 = $("#hour-17");
 var hour18 = $("#hour-18");
 
+var currentDay = $("#currentDay");
 var saveButton = $(".saveBtn");
 var currTime = dayjs().format("HH");
+var currDay = dayjs().format("MMM DD, YYYY");
 
 //var hourBlock = parseInt($element.data("id"));
 
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. 
-  // HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  
+
+  //add current date to header
+  currentDay.text(currDay);
+
   //Event listener for save button
   saveButton.on("click", function() {
     localStorage.setItem($(this).parent().attr("id"), $(this).parent().children("textarea").val());
